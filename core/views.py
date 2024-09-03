8ec33d4f29282b900551ec0733193f6c097b620f
bashilshr <120891175+bashilshr@users.noreply.github.com>
2024-09-03 21:44:35 +0545

    chore: Update templates and views for task list and detail pages

--- core/views.py
+++ core/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render,redirect
-from core.models import *
+
 from core.forms import *
+from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from core.models import task
 class tasklist (ListView):
-    quweryset = task.objects.all()
-class taskdetail (ListView):
-    quweryset = task.objects.all()
+    model = task
+    context_object_name = 'tasks'
+    template_name = "task_list.html"
+class taskdetail (DetailView):
+    model = task
+    context_object_name = 'task'
+    template_name = "task_detail.html"
+
+class taskcreate (CreateView):
+    model = task
+    fields = '__all__'
+    success_url = reverse_lazy('task')
+    template_name = "task_form.html"
--- todoapp/urls.py
+++ todoapp/urls.py
@@ -4,5 +4,9 @@ from core.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',tasklist.as_view(), name = "tasks"),
-    path('task<int:pk>',tasklist.as_view(), name = "tasks"),
+    path('task/<int:pk>/',taskdetail.as_view(), name = "task"),
+        path('task-create',taskcreate.as_view(), name = "task-create"),
+
+    
+
 ]
